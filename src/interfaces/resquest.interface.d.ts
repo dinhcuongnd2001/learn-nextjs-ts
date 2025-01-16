aabaa6dc210c0d808c8bcbb3d96fd9900fc14f37
cuongdn-lgcns <cuonglgcns@gmail.com>
2025-01-16 16:41:56 +0700

    feat: handle redirect to login page when authoried (#8)
    * :feat: handle redirect to login page when authoried
    
    * chore: remove log

--- src/interfaces/resquest.interface.d.ts
+++ src/interfaces/resquest.interface.d.ts
@@ -1,7 +1,11 @@
-import { Method } from "axios";
+import { InternalAxiosRequestConfig, Method } from "axios";
 
 export interface IAxiosAPI<T> {
     method: Method;
     url: string;
     data?: T;
+}
+
+export interface CustomAxiosConfig extends InternalAxiosRequestConfig {
+  sent?: boolean;
 }
