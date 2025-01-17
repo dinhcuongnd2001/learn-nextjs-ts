0652f89126dc6cd7b68658031ebc9ee04af82f87
cuongdn <cuongdn@lgcns.com>
2025-01-17 10:03:47 +0700

    chore: change file readme and config prettier

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -18,13 +18,24 @@ export default function Input({value, onChange, placeholder = "", label, type = 
   const handelChange = (val : string) => {
     if(onChange) onChange(val);
   }
+
+
+
+
   
   return (
     <div className="relative z-0 w-full mb-5 group">
       <input
-        onChange={(e) => handelChange(e.target.value)}
+  
+  
+  
+  
+  onChange={(e) => handelChange(e.target.value)}
         type={type}
         value={value}
+  
+  
+  
         placeholder={placeholder}
         name={`floating_${id}`}
         id={`floating_${id}`}
@@ -32,6 +43,9 @@ export default function Input({value, onChange, placeholder = "", label, type = 
       />
       {label && (
         <label htmlFor="floating_first_name" className="form-label">
+  
+  
+  
           {label}
         </label>
       )}
